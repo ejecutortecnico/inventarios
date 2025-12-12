4815197b5ff2f4a2b847027ebe5655d57c400397
ejecutortecnico <ejecutortecnico@gmail.com>
2025-12-12 15:57:09 -0500

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes, Route } from "react-router-dom";
 import Header from './componentes/Header'
 import Menu from './componentes/Menu'
-import Contenido from './componentes/Contenido'
 import AgregarEmpleados from './componentes/empleados/Agregar'
 import AgregarProductos from './componentes/productos/Agregar'
 import ListarEmpleados from './componentes/empleados/Listar'
@@ -13,26 +10,39 @@ import ListarProductos from './componentes/productos/listar'
 import ActualizarEmpleado from './componentes/empleados/actualizar';
 import List from './componentes/List';
 import BorrarEmpleado from './componentes/empleados/Borrar';
+import Login from './componentes/auth/Login';
 
 function App() {
+  const [isLogged, setIsLogged] = useState(localStorage.getItem("isLogged"))
+
+  console.log(isLogged)
+
 
   return (
     <>
-      <Header />
-      <div className="container-fluid">
-        <div className="row">
-          <Menu />
-          <Routes>
-            <Route path="/" element=<List /> />
-            <Route path="/empleados/agregar" element=<AgregarEmpleados /> />
-            <Route path="/empleados/actualizar/:id" element=<ActualizarEmpleado /> />
-            <Route path="/empleados/borrar/:id" element=<BorrarEmpleado /> />
-            <Route path="/empleados/listar" element=<ListarEmpleados /> />
-            <Route path="/productos/listar" element=<ListarProductos /> />
-            <Route path="/productos/agregar" element=<AgregarProductos /> />
-          </Routes>
-        </div >
-      </div >
+      {isLogged && (
+        <>
+          < Header />
+          <div className="container-fluid">
+            <div className="row">
+              <Menu />
+              <Routes>
+                <Route path="/" element=<List /> />
+                <Route path="/empleados/agregar" element=<AgregarEmpleados /> />
+                <Route path="/empleados/actualizar/:id" element=<ActualizarEmpleado /> />
+                <Route path="/empleados/borrar/:id" element=<BorrarEmpleado /> />
+                <Route path="/empleados/listar" element=<ListarEmpleados /> />
+                <Route path="/productos/listar" element=<ListarProductos /> />
+                <Route path="/productos/agregar" element=<AgregarProductos /> />
+              </Routes>
+            </div>
+          </div></>
+      )}
+      {!isLogged &&
+        <Routes>
+          <Route path="/" element=<Login /> />
+        </Routes>
+      }
     </>
   )
 }
---
+++ src/componentes/auth/Login.jsx
@@ -0,0 +1,42 @@
+import api from "../../api/axios";
+import { useState, useEffect } from "react";
+
+export default function Login() {
+    const [usuario, setUsuario] = useState("")
+    const [password, setPassword] = useState("")
+
+    function ingresar(e) {
+        e.preventDefault();
+        api.post("/auth/login", { usuario: usuario, password: password })
+            .then(res => crearVar())
+            .catch(err => console.error("Error:", err));
+    }
+
+    function crearVar() {
+        localStorage.setItem("isLogged", true)
+        window.location.reload();
+    }
+
+    return (
+        <>
+            <div className="container">
+                <form onSubmit={ingresar}>
+                    <div class="mb-3">
+                        <label for="usuario" class="form-label">Usuario</label>
+                        <input type="text" value={usuario} onChange={(e) => setUsuario(e.target.value)} />
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </div>
+                    <div class="form-check">
+                        <input type="checkbox" class="form-check-input" id="rememberMe" />
+                        <label class="form-check-label" for="rememberMe">Recordar</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-3">Login</button>
+                </form>
+            </div>
+        </>
+    )
+
+}
